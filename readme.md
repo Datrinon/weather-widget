f223f77d41592d0ee895a7dda072793b17d04f6c
Datrinon <dtrinh089@gmail.com>
2021-09-24 23:52:43 -0700

    Fix titling html, add readme.md demo picture

--- readme.md
+++ readme.md
@@ -1,11 +1,14 @@
 # OpenWeather API Weather Widget
 A weather widget which uses the OpenWeather API to update temperatures. 
 
+[Demo Picture](./demo.png "Sample demonstration pciture")
+
 ## Features
 - Search any location in the world.
 - View today's temperature, the next three days, or for the next 7 days. 
 - View temperatures in C or F.
 - Background colors change based on the weather condition and time of day of the location.
+- Remembers your last searched location.
 
 ## Usage
 - To get a weather widget on a webpage, simply invoke the constructor with your API key.
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import '../css/reset.css';
 import '../css/index.css';
 import '../css/weather.css';
 //js
+import Utility from './utility';
 import WeatherWidget from './weather';
 import component from './component';
 
@@ -13,11 +14,16 @@ import "@fortawesome/fontawesome-free/css/all.css";
 (function main() {
   const img = component.img("https://openweathermap.org/themes/openweathermap/assets/img/logo_white_cropped.png", "ow-logo");
   const header = component.heading("API Widget Demo", 1, "page-title");
-  const header2 = component.heading("by", 1, "author");
-  const authorLink = Utility.createElement("a", "gh-link");
+  const authorHeader = component.heading("by ", 1, "author");
+
+  const authorLink = Utility.createElement("a", "author-link");
   authorLink.textContent = "Dan T.";
-  authorLink.setAttribute("href", "")
+  authorLink.setAttribute("href", "https://github.com/Datrinon/weather-widget");
+  authorLink.setAttribute("title", "Visit GitHub");
+  authorHeader.append(authorLink);
+
+
   const weather = new WeatherWidget("f6345d1dad5d9efead6cd5394a064987");
 
-  document.body.append(img, header, weather.widget);
+  document.body.append(img, header, authorHeader, weather.widget);
 })();
